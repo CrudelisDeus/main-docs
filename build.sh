977a2b42cc3c284e911bb66be6e51865fa09105b
Dmitry <mathmainfunction@gmail.com>
2025-04-03 16:08:45 +0300

    change build

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ vitepress build docs
 # cv cp
 mkdir -p docs/.vitepress/dist/pet/img/index
 cp -r docs/pet/img/index/*.pdf docs/.vitepress/dist/pet/img/index
+
+# analitics
+cp -R *.html docs/.vitepress/dist
