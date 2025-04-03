1b7a814fdb10f31875e52f297a21f905af3ed392
Dmitry <mathmainfunction@gmail.com>
2025-04-03 16:14:25 +0300

    added robots xd

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ vitepress build docs
 # cv cp
 mkdir -p docs/.vitepress/dist/pet/img/index
 cp -r docs/pet/img/index/*.pdf docs/.vitepress/dist/pet/img/index
+
+# robots && sitemap
+cp robots.txt docs/.vitepress/dist
