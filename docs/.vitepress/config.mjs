0c27539a2c71502b26a74772d1000318963f3b98
Dmitry <mathmainfunction@gmail.com>
2025-04-03 17:38:16 +0300

    change google

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -77,27 +77,26 @@ export default withMermaid(
     },
 
     head: [
-        ['script', { src: 'https://cdn.tailwindcss.com' }],
-        ['script', {
-          async: true,
-          src: 'https://www.googletagmanager.com/gtag/js?id=G-858GSPPL6'
-        }],
+      ['script', { src: 'https://cdn.tailwindcss.com' }],
 
-        ['script', {}, `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-858GSPPL6');
-        `]
-        ['meta', { name: 'robots', content: 'index, follow' }],
-        ['link', { rel: 'icon shortcut', href: './logo-short.svg' }],
-          ['script', {}, `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-T5KFBGF7'); document.documentElement.classList.add('dark');
-        localStorage.setItem('vitepress-theme-appearance', 'dark');`]
-        ],
+      ['script', {
+        async: true,
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-858GSPPL6'
+      }],
+      ['script', {}, `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-858GSPPL6');
+      `],
+
+      ['meta', { name: 'robots', content: 'index, follow' }],
+      ['link', { rel: 'icon shortcut', href: './logo-short.svg' }],
+      ['script', {}, `
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('vitepress-theme-appearance', 'dark');
+      `]
+    ],
 
     // https://vitepress.dev/reference/default-theme-config
     themeConfig: {
