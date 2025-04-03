27aa39d5923b87e5c734f3d2cd81fb8ed35fd6be
Dmitry <mathmainfunction@gmail.com>
2025-04-03 17:25:02 +0300

    test google

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -80,8 +80,13 @@ export default withMermaid(
         ['script', { src: 'https://cdn.tailwindcss.com' }],
         ['meta', { name: 'robots', content: 'index, follow' }],
         ['link', { rel: 'icon shortcut', href: './logo-short.svg' }],
-        ['script', {}, `document.documentElement.classList.add('dark');
-        localStorage.setItem('vitepress-theme-appearance', 'dark');`]],
+          ['script', {}, `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-T5KFBGF7'); document.documentElement.classList.add('dark');
+        localStorage.setItem('vitepress-theme-appearance', 'dark');`]
+        ],
 
     // https://vitepress.dev/reference/default-theme-config
     themeConfig: {
--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -2,12 +2,17 @@
 import DefaultTheme from 'vitepress/theme';
 import Panzoom from './components/Panzoom.vue';
 import NavigationLinks from './components/NavigationLinks.vue';
+import GTMNoScript from './components/GTMNoScript.vue'; // 1. Импортируем компонент
 
 const { Layout } = DefaultTheme;
 </script>
 
 <template>
   <Layout>
+    <template #layout-top>
+      <GTMNoScript />
+    </template>
+
     <template #aside-outline-after>
       <NavigationLinks />
     </template>
